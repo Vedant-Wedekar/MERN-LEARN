aa87ce8ded42dc4104ffaed87e0afffe051f2aa8
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-15 22:47:37 +0530

    error solving

--- server/controllers/auth-con.js
+++ server/controllers/auth-con.js
@@ -2,8 +2,15 @@ const home = async (req, res) => {
     try { res.status(200).send('this is page throught the router ');}
     catch (error) { console.error(error); }
 }
+
+const profile = async (req, res) => {
+    try {console.log(req.body); 
+         res.status(200).json({message:req.body});
+        }
+    catch (error) { console.error(error); }
+}
 //this function is mapped to the home route in auth.js file. It responds with a status 200 and a message when accessed through the route '/profile'.
-module.exports = {home}; //exporting the function for use in other files.
+module.exports = {home,profile}; //exporting the function for use in other files.
 
 
 
