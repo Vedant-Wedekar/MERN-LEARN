df0b2f669e84bbecefaba09efed7e59c137aad13
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-15 22:47:51 +0530

    error solving

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {home} = require('../controllers/auth-con');
-router.route('/').get(home); //mapping the home route to the home function.
+const {home,profile} = require('../controllers/auth-con');
 
+router.route('/').get(home); //mapping the home route to the home function.
+router.route('/profile').post(profile); //mapping the profile route to the profile function.
 module.exports = router; //exporting the router for use in other files.
 
 
