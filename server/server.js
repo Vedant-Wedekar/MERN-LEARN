a5290d84fc30be2e924d8812bcc6ee22149ac24f
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-15 22:47:21 +0530

    error solving

--- server/server.js
+++ server/server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const app = express();
 const router = require('./routes/auth');
 
-app.use('/profile', router); // This means all routes inside `router` are prefixed with `/profile`
-
-const port = 6000; 
+app.use('/', router); // Use only one base route
+const port = 4000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-
