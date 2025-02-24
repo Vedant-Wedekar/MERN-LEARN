210ca21256ec645f17531473337629a3f77aaebb
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-24 20:28:32 +0530

    username issue

--- server/controllers/auth-con.js
+++ server/controllers/auth-con.js
@@ -8,13 +8,13 @@ const home = async (req, res) => {
 const profile = async (req, res) => {
    
     try { res.status(200).send('this is page throught the profile ');
-        const {user,email,phone , password } = req.body
+        const {username,email,phone , password } = req.body
 
         const userExist = await User.findOne({email: email})
         if(userExist){
             return res.status(400).json({message: "User already exist"})
         }
-        await User.create({username: user, email: email, phone: phone, password: password})
+        await User.create({username: username, email: email, phone: phone, password: password})
 
 
 
